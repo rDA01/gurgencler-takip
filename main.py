ad7d3d9998752dbb7c12897aef05dfd9fe57e749
rDA01 <ardatevek@gmail.com>
2024-07-18 17:43:51 +0300

    initial commit

--- main.py
+++ main.py
@@ -8,8 +8,15 @@ from data.entities.product import Product
 from data.repositories.productRepository import ProductRepository
 from service.productService import ProductService
 from service.telegramService import TelegramService
-
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import chrome
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
 
 class LoggingConfigurator:
     def __init__(self):
@@ -31,23 +38,54 @@ class GatherPagesItems(LoggingConfigurator):
         self.item_count=0
         self.product_repo = product_repo
 
-    async def gather_page_number(self, base_url, i):
+    async def gather_page_number(self, base_url):
         try:
-            response = requests.get(base_url + str(i))
+            response = requests.get(base_url)
             if response.status_code == 200:
-                soup = BeautifulSoup(response.content, 'html.parser')
-                divs = soup.find_all('div', class_='p-card-chldrn-cntnr card-border')
-                for div in divs:
-                    h3 = div.find('h3', class_='prdct-desc-cntnr-ttl-w')
-                    a = div.find('a', href=True)
-                    prc_box_dscntd = div.find('div', class_='prc-box-dscntd')
-                    if h3 and a and prc_box_dscntd:
-                        title = h3.get_text(strip=True)
-                        print(title)
-                        href = a['href']
 
+                PATH = "C:\Program Files (x86)\chromedriver.exe"
+                options = webdriver.ChromeOptions()
+                options.add_experimental_option("detach", True)
+                cService = webdriver.ChromeService(executable_path=PATH)
+                driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+                driver.get(base_url)
+                time.sleep(5)
+                button = driver.find_element(By.ID, "button-1580496494")
+                button.click()
+                time.sleep(5)
+                
+                for i in range(10):
+                    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                    
+
+                    i += 1
+                    time.sleep(1)
+                
+                time.sleep(5)
+                soup = BeautifulSoup(driver.page_source, 'html.parser')
+                divs = soup.find_all('div', class_='product-item-info')
+
+                print(len(divs))
+                
+                for div in divs:
+                    try:
+                        container = div.find('div',class_='product details product-item-details eln-product-item-details')
+                        details = container.find('a',class_="product-item-link")
+                        prc_box_dscntd = container.find('div', class_='price-box price-final_price')
+                    
+                        prc_span = prc_box_dscntd.find('span', class_="price-container price-final_price tax weee")
+                        span = prc_span.find('span',class_="price-wrapper")
+                        price = span.find('p',class_="special-price")
+                        p = price.find('span',class_="price")
+                    except:
+                        pass
+
+                    if details and p:
+                        title = details.get_text(strip=True)                       
+                        href = details['href']
+                                                
                         item =  self.product_repo.get_product_by_link(href)
-                        price_text = prc_box_dscntd.text.strip()
+                        price_text = p.text.strip()
                         price_text = price_text.replace('.', '').replace(',', '.')  # Replace comma with dot
                         price = float(''.join(filter(lambda x: x.isdigit() or x == '.', price_text)))                        
                         
@@ -62,11 +100,7 @@ class GatherPagesItems(LoggingConfigurator):
                         
                         print("Incomplete data found in div, skipping.")
                 
-                div_count = len(divs)
-                if div_count != 24:
-                    self.item_count = (i * 24) + div_count
-                    self.page_count = i
-                    return False
+          
             else:
                 print("Failed to retrieve page:", response.status_code)
                 return False
@@ -78,36 +112,23 @@ class GatherPagesItems(LoggingConfigurator):
 
     async def gather_page_numbers(self):
         base_url = self.base_url
-        loop_var = True
-        i = 1
-        while loop_var:
-            #threads = []
-            #for _ in range(50):
-            #    t = threading.Thread(target=self.gather_page_number, args=(base_url, i))
-            #    t.start()
-            #    threads.append(t)
-            #    i += 1
-            #for t in threads:
-            #    t.join()
-
-            loop_var = await self.gather_page_number(base_url, i)
-            i = i + 1
+        
+        await self.gather_page_number(base_url)
 
 async def Main():
     product_repo = ProductRepository()
 
-    smartphones = GatherPagesItems(product_repo,"https://www.trendyol.com/akilli-cep-telefonu-x-c109460?pi=")
+    smartphones = GatherPagesItems(product_repo,"https://www.gurgencler.com.tr/iphone?filters[quantity_and_stock_status]=yes")
     
     await smartphones.gather_page_numbers()
 
-    dysonproducts = GatherPagesItems(product_repo,"https://www.trendyol.com/dyson-dik-supurge-x-b102989-c109454?pi=")
-    await dysonproducts.gather_page_numbers()
     
-    telegram_service = TelegramService(bot_token='7472974345:AAGLXNzPyrik5KZJP3EQgLS_XzPCi9w7E0E', chat_id='-1002246478070')
+    telegram_service = TelegramService(bot_token='7393980187:AAGJHwoW6DY98jZOvTzdq0o7Ojt8X1VO28Q', chat_id='-1002203530212')
 
     productService = ProductService(product_repo, telegram_service)
     
     while True:
         await productService.updateProduct()
+    
 
 asyncio.run(Main())
--- service/productService.py
+++ service/productService.py
@@ -12,27 +12,34 @@ class ProductService:
     def __init__(self, repository: ProductRepository, telegram_service: TelegramService):
         self.repository = repository
         self.telegram_service = telegram_service
-        self.base_url = "https://trendyol.com"
+        self.base_url = "https://www.gurgencler.com.tr/"
     async def updateProduct(self):
         links = self.repository.get_all_product_links()
 
         for link in links:
 
-            response = requests.get(str(self.base_url) + str(link))
+            response = requests.get(str(link))
             if response.status_code == 200:
                 soup = BeautifulSoup(response.content, 'html.parser')
-                product_detail_div = soup.find('div', class_='product-detail-wrapper')
-
+                product_options_div = soup.find('div', class_='product-options-bottom')
+                product_details = product_options_div.find('div',class_="mnm-after-final-price")
+                product_detail_div = product_details.find('div',class_="price-box price-final_price")
                 if product_detail_div:
-                    price_span = product_detail_div.find('span', class_='prc-dsc')
-
+                    span = product_detail_div.find('span',class_="normal-price")
+                    span = span.find('span',class_="price-container price-final_price tax weee")
+                    price_span = span.find('span', class_='price')
+                    print(price_span)
                     if price_span:
+                        
                         price_text = price_span.text.strip()
                         price_text = price_text.replace('.', '').replace(',', '.')  # Replace comma with dot
                         price_numeric = float(''.join(filter(lambda x: x.isdigit() or x == '.', price_text)))
                         price_numeric = Decimal(price_numeric)
                         product = self.repository.get_product_by_link(link)
-
+                        print(product.title)
+                        print("database: ",product.price)
+                        print("web: ", price_numeric)
+                        print(str(link))
                         if product:
                             if product.price != price_numeric:
                                 print("existing price: ", product.price, '\n', "new price: ", price_numeric)
@@ -41,17 +48,17 @@ class ProductService:
                                 
                                 price_numeric = Decimal(price_numeric)
                                  
-                                isInstallment = Decimal(price_numeric) <= Decimal(old_price) * Decimal(0.92) 
+                                
                                 product.price = Decimal(price_numeric)
                                 self.repository.update_product(product)
-
+                                isInstallment = Decimal(price_numeric) <= Decimal(old_price) * Decimal(0.92) 
                                 if(isInstallment):
                                     print("installment catched, product link: ", product.link)
                                     installment_rate = ((old_price - Decimal(price_numeric)) / old_price) * 100
                                     old_price = "{:.2f}".format(old_price) 
                                     price_numeric = "{:.2f}".format(price_numeric)
                                     installment_rate = "{:.1f}".format(installment_rate)
-                                    message = f"{str("m.trendyol.com") + str(link)} linkli, {product.title} başlıklı ürünün fiyatında indirim oldu. Önceki fiyat: {old_price}, Yeni fiyat: {price_numeric}. İndirim oranı: %{installment_rate}"
+                                    message = f"{str(link)} linkli, {product.title} başlıklı ürünün fiyatında indirim oldu. Önceki fiyat: {old_price}, Yeni fiyat: {price_numeric}. İndirim oranı: %{installment_rate}"
 
                                     await self.telegram_service.send_message(message)
                                    
